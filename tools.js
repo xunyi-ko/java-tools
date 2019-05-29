d72ffd7ce21a04ba2f950968c4be789e534aca29
xunyi-ko <39740915+xunyi-ko@users.noreply.github.com>
2019-05-29 14:34:50 +0800

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,3 +1,18 @@
 jQuery.download = function(url, method){
     jQuery('<form action="'+url+'" method="'+(method||'post')+'">' + '</form>').appendTo('body').submit().remove();
 };
+
+// 在修改信息的页面调用此方法，在修改了信息后，离开页面时会被拦截。
+function onBeforeUnload(){
+    $("input,textarea").bind("change",function(){
+        window.onbeforeunload = function (e) {
+            var e = window.event || e;
+            e.returnValue = ("确定离开当前页面吗？(修改的信息不会保留)");
+        }
+    })
+}
+
+// 去除离开页面会拦截的事件
+function onSaveDispatcher(){
+    window.onbeforeunload = function (e) {}
+}
