706645a293cc86ec7feff053dc73fb019e3badbe
xunyi-ko <39740915+xunyi-ko@users.noreply.github.com>
2019-07-02 17:01:37 +0800

    Create Out.java

---
+++ utils/Out.java
@@ -0,0 +1,60 @@
+import java.io.PrintStream;
+
+public class Out{
+    private PrintStream out = System.out;
+    
+    public static void print(Object o){
+        out.print(o);
+    }
+    public static void print(byte i){
+        out.print(i);
+    }
+    public static void print(short i){
+        out.print(i);
+    }
+    public static void print(int i){
+        out.print(i);
+    }
+    public static void print(long i){
+        out.print(i);
+    }
+    public static void print(char i){
+        out.print(i);
+    }
+    public static void print(float i){
+        out.print(i);
+    }
+    public static void print(double i){
+        out.print(i);
+    }
+    public static void print(boolean i){
+        out.print(i);
+    }
+    public static void println(Object o){
+        out.println(o);
+    }
+    public static void println(byte i){
+        out.println(i);
+    }
+    public static void println(short i){
+        out.println(i);
+    }
+    public static void println(int i){
+        out.println(i);
+    }
+    public static void println(long i){
+        out.println(i);
+    }
+    public static void println(char i){
+        out.println(i);
+    }
+    public static void println(float i){
+        out.println(i);
+    }
+    public static void println(double i){
+        out.println(i);
+    }
+    public static void println(boolean i){
+        out.println(i);
+    }
+}
